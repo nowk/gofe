fac6ff6ee0e2c8a392b7bc5a224f0aabfcd7044b
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-04-25 18:37:03 -0400

    Refactor organize

--- gofe.go
+++ gofe.go
@@ -37,23 +37,41 @@ func New(t Testing, s ...Steps) *Feature {
 	}
 }
 
-func (f *Feature) Step(name string, v ...interface{}) {
-	for _, s := range f.Steps {
-		if fn, ok := s[name]; ok {
-			t := reflect.TypeOf(fn)
-			stepFunc := reflect.ValueOf(fn).Call([]reflect.Value{
-				reflect.ValueOf(f.t),
-			})
+func (f Feature) findStep(name string) (reflect.Value, int, error) {
+	var stepFunc StepFunc
 
-			t = reflect.TypeOf(stepFunc[0].Interface())
+	for _, v := range f.Steps {
+		if fn, ok := v[name]; ok {
+			stepFunc = fn
 
-			args := make([]reflect.Value, t.NumIn())
-			for i := 0; i < len(args); i++ {
-				args[i] = reflect.ValueOf(v[i])
-			}
-
-			stepFunc[0].Call(args)
-			return
+			break // step found
 		}
 	}
+	if stepFunc == nil {
+		return reflect.Value{}, 0, nil
+	}
+
+	s := reflect.ValueOf(stepFunc).Call([]reflect.Value{
+		reflect.ValueOf(f.t),
+	})
+
+	// TODO check count for len == 1
+
+	fn := s[0]
+	to := reflect.TypeOf(fn.Interface())
+	return fn, to.NumIn(), nil
+}
+
+func (f Feature) Step(name string, v ...interface{}) {
+	fn, n, err := f.findStep(name)
+	if err != nil {
+		// TODO handle
+	}
+
+	argv := make([]reflect.Value, n)
+	for i := 0; i < len(argv); i++ {
+		argv[i] = reflect.ValueOf(v[i])
+	}
+
+	fn.Call(argv)
 }
